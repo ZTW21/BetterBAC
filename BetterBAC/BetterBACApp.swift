6cdb6a279d35661b444f2b6ca3250ded170b73d5
ZTW21 <ztwilson2002@gmail.com>
2024-06-04 18:49:53 -0400

    Initial Commit

---
+++ BetterBAC/BetterBACApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterBACApp.swift
+//  BetterBAC
+//
+//  Created by Zack Wilson on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterBACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
